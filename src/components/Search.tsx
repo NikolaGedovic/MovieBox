be15287013432c40b4ca730ea684decc0f6cf18e
Nikola Gedović <nikola.gedovic@gmail.com>
2025-11-25 19:07:29 +0100

    Fixing the color of the btn.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -8,7 +8,7 @@ export default function Search() {
         placeholder="Search your movie..."
         className="bg-indigo-50 px-4 w-xl mr-5 py-2 rounded"
       />
-      <button className="text-indigo-50 bg-sky-700 rounded px-7 py-2 font-bold hover:bg-sky-900 hover:cursor-pointer active:bg-sky-600">
+      <button className="text-indigo-50 bg-teal-700 rounded px-7 py-2 font-bold hover:bg-teal-600 hover:cursor-pointer active:bg-teal-800">
         Search
       </button>
     </div>
