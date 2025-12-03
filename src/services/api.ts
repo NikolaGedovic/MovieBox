88b782bf67fdd3657e03ffada6cfb0bc3eb4fbac
Nikola Gedović <nikola.gedovic@gmail.com>
2025-12-03 17:44:37 +0100

    fetching the api for the movies

---
+++ src/services/api.ts
@@ -0,0 +1,18 @@
+interface Movie {
+  Title: string;
+  Year: string;
+  Genre: string;
+  Runtime: string;
+  Plot: string;
+  Poster: string;
+}
+
+export default async function MovieApiFetch(
+  movieTitle: string
+): Promise<Movie> {
+  const apiKey: string = "d64ee086";
+  const movieUrl: string = `http://www.omdbapi.com/?apikey=${apiKey}&t=${movieTitle}`;
+  const res: Response = await fetch(movieUrl);
+  const data: Movie = await res.json();
+  return data;
+}
